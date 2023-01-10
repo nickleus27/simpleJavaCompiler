7c85d02bf6563cc142e595ede423dabd19b7e357
nickleus27 <nickleus27@gmail.com>
2023-01-10 14:51:49 -0800

    test9

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -313,6 +313,33 @@ class Test_sJava_Files(unittest.TestCase):
                 remove(sjout)
             if path.isfile(sjs):
                 remove(sjs)
+
+    def test_test9(self):
+        self.setup()
+        test8 = path.join(self.dir, "test9.sjava")
+        sjs = path.join(self.dir, "test9.sjava.s")
+        sjo = path.join(self.dir, "test9.sjava.o")
+        sjout = path.join(self.dir, "test9.sjava.out")
+        tmp8 = path.join(self.dir, "temp9.txt")
+        test_txt = path.join(self.dir, "test9.txt")
+        if path.isfile(test8):
+            exitStatus = run([self.sjc_exe, test8]).returncode
+            self.assertEqual(exitStatus, 0)
+            if path.isfile(sjout):
+                with open(tmp8, 'w') as outfile:
+                    run(path.join(self.exe, sjout), stdout=outfile)
+                    outfile.close()
+            if path.isfile(tmp8):
+                self.assertTrue(filecmp.cmp(tmp8, test_txt))
+                remove(tmp8)
+            else:
+                self.assertTrue(False)
+            if path.isfile(sjo):
+                remove(sjo)
+            if path.isfile(sjout):
+                remove(sjout)
+            if path.isfile(sjs):
+                remove(sjs)
         self.teardown()
 
 if __name__ == '__main__':
